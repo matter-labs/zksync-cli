48c7b628df77fddb9a339bda8e39c048aa01b04b
Antonio <tonioufa@gmail.com>
2022-10-25 11:57:07 +0100

    current directory with .

--- bin/index.js
+++ bin/index.js
@@ -18,7 +18,7 @@ const runCommand = (command) => {
   return true;
 };
 
-const projectName = process.argv[2] || '';
+const projectName = process.argv[2] || '.';
 
 // clones repo inside the given project name folder
 const cloneGitTemplate = `git clone --depth 1 https://github.com/matter-labs/zksync-hardhat-template ${projectName}`;
