37d432e229e9b570bb5ce42b6c3062afbd10a88a
Ramon Canales <emaildoramon@gmail.com>
2023-02-26 08:55:42 -0700

    avoiding app breaking if analytics is not configured properly

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,12 +1,17 @@
 import { machineId } from 'node-machine-id';
 import RudderAnalytics from "@rudderstack/rudder-sdk-node"
 
-const client: RudderAnalytics = new RudderAnalytics(process.env.RUDDER_STACK_KEY!, {
+let client: RudderAnalytics | undefined;
+try {
+    client = new RudderAnalytics(process.env.RUDDER_STACK_KEY!, {
     dataPlaneUrl: process.env.RUDDER_STACK_DATAPLANE_URL!, 
 })
+} catch (e) {
+}
 
 export const track = async (event: string, properties?: any) => {
-    client.track({
+    if(!client) return;
+    client!.track({
         userId: await machineId(), 
         event,
         properties,
