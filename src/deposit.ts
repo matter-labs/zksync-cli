f0c4dacd96ad0b3d35bde64c0fe5e9bd06206757
mm-zk <mm@matterlabs.dev>
2023-04-03 14:37:38 +0200

    added support for other networks

--- src/deposit.ts
+++ src/deposit.ts
@@ -9,6 +9,14 @@ export default async function () {
 
   const questions: QuestionCollection = [
     {
+      message: "Network:",
+      name: "network",
+      type: "list",
+      choices: ["goerli", "mainnet", "localnet"],
+      default: "goerli"
+    },
+
+    {
       message: "Address to deposit funds to:",
       name: "to",
       type: "input",
@@ -29,13 +37,40 @@ export default async function () {
   const results: Answers = await inquirer.prompt(questions);
 
   console.log(
-    chalk.magentaBright(`Depositing ${results.amount}ETH to ${results.to}`)
+    chalk.magentaBright(`Depositing ${results.amount}ETH to ${results.to} on ${results.network}`)
   );
+  var ethProviderUrl;
+  var zksyncProviderUrl;
+  var etherScanUrl;
+  var zkSyncExplorerUrl;
+
+  switch (results.network) {
+    case "mainnet":
+      ethProviderUrl = "mainnet"
+      zksyncProviderUrl = "zksync2.zksync.dev"
+      etherScanUrl = "https://etherscan.io/tx/"
+      zkSyncExplorerUrl = "https://explorer.zksync.io/address/"
+      break;
+    case "goerli":
+      ethProviderUrl = "goerli"
+      zksyncProviderUrl = "https://zksync2-testnet.zksync.dev"
+      etherScanUrl = "https://goerli.etherscan.io/tx/"
+      zkSyncExplorerUrl = "https://goerli.explorer.zksync.io/address/"
+      break;
+    case "localnet":
+      ethProviderUrl = "http://127.0.0.1:8545"
+      zksyncProviderUrl = "http://127.0.0.1:3050"
+      etherScanUrl = "L1 transaction: "
+      zkSyncExplorerUrl = "L2 address:"
+      break;
+    default:
+      throw "Unsupported network ${results.network}";
+  }
 
   // Initialize the wallet.
-  const L1Provider = ethers.getDefaultProvider("goerli");
+  const L1Provider = ethers.getDefaultProvider(ethProviderUrl);
 
-  const zkSyncProvider = new Provider("https://zksync2-testnet.zksync.dev");
+  const zkSyncProvider = new Provider(zksyncProviderUrl);
   const wallet = new Wallet(results.key, zkSyncProvider, L1Provider);
 
   // Deposit funds to L2
@@ -47,7 +82,7 @@ export default async function () {
 
   console.log(chalk.magentaBright(`Transaction submitted 💸💸💸`));
   console.log(
-    chalk.magentaBright(`https://goerli.etherscan.io/tx/${depositHandle.hash}`)
+    chalk.magentaBright(`${etherScanUrl}${depositHandle.hash}`)
   );
   console.log(
     chalk.magentaBright(
@@ -56,7 +91,7 @@ export default async function () {
   );
   console.log(
     chalk.magentaBright(
-      `To check the latest transactions of this wallet on zkSync, visit: https://goerli.explorer.zksync.io/address/${results.to}`
+      `To check the latest transactions of this wallet on zkSync, visit: ${zkSyncExplorerUrl}${results.to}`
     )
   );
 
