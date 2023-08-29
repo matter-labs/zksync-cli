02bcd62010b6a86520a411a3a6c8251bbb62c483
Jack Hamer <jackhamer0990@gmail.com>
2023-08-29 20:50:01 +0300

    fix: analytics ts errors

--- src/analytics.ts
+++ src/analytics.ts
@@ -1,17 +1,22 @@
 import RudderAnalytics from "@rudderstack/rudder-sdk-node";
 import { machineId } from "node-machine-id";
 
+import type { apiObject } from "@rudderstack/rudder-sdk-node";
+
 let client: RudderAnalytics | undefined;
 try {
   client = new RudderAnalytics(process.env.RUDDER_STACK_KEY!, {
     dataPlaneUrl: process.env.RUDDER_STACK_DATAPLANE_URL!,
   });
-} catch (e) {}
+} catch {
+  // ignore
+}
 
-export const track = async (event: string, properties?: any) => {
+export const track = async (event: string, properties?: unknown) => {
+  if (!client || process.env.NO_TRACKING) return;
+
+  // eslint-disable-next-line no-async-promise-executor
   return new Promise<void>(async (resolve) => {
-    if (!client || process.env.NO_TRACKING) return;
-
     const raceFunction = setTimeout(() => {
       resolve();
     }, 1000);
@@ -19,7 +24,7 @@ export const track = async (event: string, properties?: any) => {
       {
         userId: await machineId(),
         event,
-        properties,
+        properties: properties as apiObject,
       },
       () => {
         clearTimeout(raceFunction);
