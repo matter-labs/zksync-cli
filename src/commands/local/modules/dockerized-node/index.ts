6f48a95d11e0bd680a390ad86bbbeee877741f88
Jack Hamer <47187316+JackHamer09@users.noreply.github.com>
2023-09-15 17:51:23 +0300

    fix: start failing when files aren't installed (#52)

--- src/commands/local/modules/dockerized-node/index.ts
+++ src/commands/local/modules/dockerized-node/index.ts
@@ -9,7 +9,7 @@ import {
   composeStop,
   composeUp,
 } from "../../../../utils/docker";
-import { cloneRepo } from "../../../../utils/git";
+import { cloneRepo, isRepoCloned } from "../../../../utils/git";
 import Logger from "../../../../utils/logger";
 
 import type { Config } from "../../config";
@@ -27,20 +27,25 @@ export default class SetupModule extends Module {
     );
   }
 
+  justInstalled = false;
+
   git = "https://github.com/matter-labs/local-setup.git";
   composeFile = path.join(this.folder, "docker-compose.yml");
 
   async isInstalled() {
+    if (!isRepoCloned(this.folder)) return false;
     return (await composeStatus(this.composeFile, this.folder)).length ? true : false;
   }
 
   async install() {
     await cloneRepo(this.git, this.folder);
     await composeCreate(this.composeFile, this.folder);
-    Logger.warn("First start may take a while until zkSync node is actually running, please be patient...");
+    this.justInstalled = true;
   }
 
   async isRunning() {
+    if (await this.isInstalled()) return false;
+
     return (await composeStatus(this.composeFile, this.folder)).some(({ status }) => {
       return status === "running" || status === "restarting";
     });
@@ -54,17 +59,23 @@ export default class SetupModule extends Module {
     Logger.info(`${this.name} ready:
  - L1 RPC URL: http://localhost:8545
  - L2 RPC URL: http://localhost:3050`);
+    if (this.justInstalled) {
+      Logger.warn(" - First start may take a while until zkSync node is actually running, please be patient...");
+    }
   }
 
   async stop() {
+    if (!(await this.isInstalled())) return;
     await composeStop(this.composeFile, this.folder);
   }
 
   async clean() {
+    if (!(await this.isInstalled())) return;
     await composeDown(this.composeFile, this.folder);
   }
 
   async restart() {
+    if (!(await this.isInstalled())) return;
     await composeRestart(this.composeFile, this.folder);
   }
 }
