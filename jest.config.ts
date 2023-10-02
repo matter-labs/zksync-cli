babdeadc22721ea10e11364481e3d4fa8ebb0505
Jack Hamer <jackhamer0990@gmail.com>
2023-10-02 16:31:01 +0300

    fix: remove tests

--- jest.config.ts
+++
@@ -1,5 +0,0 @@
-export default {
-  preset: "ts-jest",
-  testMatch: ["<rootDir>/src/**/*.spec.ts"],
-  setupFilesAfterEnv: ["<rootDir>/src/test-utils/jestGlobalSetup.ts", "<rootDir>/src/test-utils/requestInterceptor.ts"],
-};
