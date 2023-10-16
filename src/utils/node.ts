e93b952c781c7c2da9e717adab2bda2ef213e730
Jack Hamer <jackhamer0990@gmail.com>
2023-10-16 18:35:08 +0300

    fix: check node version

--- src/utils/node.ts
+++ src/utils/node.ts
@@ -5,9 +5,12 @@ let nodeVersion: string | undefined;
 export const getNodeVersion = async () => {
   if (nodeVersion) return nodeVersion;
   try {
-    const version = await executeCommand("node --version", { silent: true });
-    nodeVersion = version.trim().slice(1);
-    return nodeVersion;
+    const version = (await executeCommand("node --version", { silent: true })).trim();
+    const vIndex = version.indexOf("v");
+    if (vIndex === -1) {
+      return version;
+    }
+    return version.slice(vIndex + 1);
   } catch {
     throw new Error("Node.js is not installed. Download: http://nodejs.org");
   }
