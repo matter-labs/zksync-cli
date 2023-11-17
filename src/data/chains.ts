c1ea3cc1320d389b548f2e04a0c91dfe82bcdf71
IAvecilla <iavecilla@fi.uba.ar>
2023-11-17 20:40:11 +0300

    Update rpc url for in memory and L1 nodes

--- src/data/chains.ts
+++ src/data/chains.ts
@@ -43,7 +43,7 @@ export const l2Chains: L2Chain[] = [
     id: 260,
     name: "Local In-memory node",
     network: "local-in-memory",
-    rpcUrl: "http://localhost:8011",
+    rpcUrl: "http://127.0.0.1:8011",
   },
   {
     id: 270,
@@ -54,7 +54,7 @@ export const l2Chains: L2Chain[] = [
       id: 9,
       name: "L1 Local",
       network: "l1-local",
-      rpcUrl: "http://localhost:8545",
+      rpcUrl: "http://127.0.0.1:8545",
     },
   },
 ];
