975fd31bac336cf8e2a5afbf52fc9596e6052bcf
Milton <milton.scuderi@lambdaclass.com>
2023-11-22 23:03:12 -0300

    Add Address conversion

--- src/commands/contract/call.ts
+++ src/commands/contract/call.ts
@@ -1,18 +1,82 @@
 import inquirer from "inquirer";
 
 import Program from "./command.js";
-import { DefaultOptions, accountOption, chainOption, zeekOption } from "../../common/options.js";
-import { l2Chains } from "../../data/chains.js";
-import { bigNumberToDecimal } from "../../utils/formatters.js";
-import { getL2Provider, optionNameToParam } from "../../utils/helpers.js";
+import { optionNameToParam } from "../../utils/helpers.js";
+import { Option } from "commander";
 import Logger from "../../utils/logger.js";
-import { isAddress } from "../../utils/validators.js";
-import zeek from "../../utils/zeek.js";
 
-type CallOptions = DefaultOptions & {
-    chain?: string;
-    address?: string;
+
+const contractOption = new Option("--conctract, --contract <someContract(arguments)>", "contract to call");
+const functionOption = new Option("--f, --function <someFunction(arguments)>", "function to call");
+const argumentsOption = new Option("--args, --arguments <argument list>", "arguments to call");
+const dataOption = new Option("--d, --data <someData(arguments)>", "data to call the functions");
+
+
+type CallOptions = {
+    contract?: string; 
     function?: string;
+    args?: string;
     data?: string;
+    // chain_id?: string;
   };
-  
+
+
+  function convertAddress(address: string = '') {
+        let prefix = address.slice(0,2);
+        if (prefix === "0x") {
+            let slice = address.slice(2);
+            return Uint8Array.from(Buffer.from(slice, "hex"));    
+        }
+        return Uint8Array.from(Buffer.from(address, "hex"));
+  }
+
+  export const handler = async (options: CallOptions) => {
+    try {
+        const answers = await inquirer.prompt(
+            [  
+                {
+                    message: contractOption.description,
+                    name: optionNameToParam(contractOption.long!),
+                    type: "input",
+                    required: true,
+                },
+                // {
+                //     message: functionOption.description,
+                //     name: optionNameToParam(functionOption.long!),
+                //     type: "input",
+                //     required: true,
+                // },
+                // {
+                //     message: argumentsOption.description,
+                //     name: optionNameToParam(argumentsOption.long!),
+                //     type: "input",
+                //     required: true,
+                // },
+                // {
+                //     message: dataOption.description,
+                //     name: optionNameToParam(dataOption.long!),
+                //     type: "input",
+                //     required: true,
+                // },
+              ],
+              options
+            );
+
+            options = {
+                ...options,
+                ...answers,
+              };
+          
+            let contract = convertAddress(options.contract);
+            console.log(contract);
+
+    } catch(error) {
+        Logger.error("There was an error while performing function call");
+        Logger.error(error);
+    }
+  }
+
+  Program.command("call")
+    .addOption(contractOption)
+    .description("Call a contract function")
+    .action(handler);
