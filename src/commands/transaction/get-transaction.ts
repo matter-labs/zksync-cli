18cdbe4e384aae6193d3cd17348cb699b9966ab2
juan.mv <juan@lambdaclass.com>
2023-11-27 10:47:46 -0300

    Add get transaction command

--- src/commands/transaction/get-transaction.ts
+++ src/commands/transaction/get-transaction.ts
@@ -1,32 +1,15 @@
 import inquirer from "inquirer";
 
 import Program from "./command.js";
-import {
-  amountOptionCreate,
-  chainOption,
-  l1RpcUrlOption,
-  l2RpcUrlOption,
-  privateKeyOption,
-  recipientOptionCreate,
-  zeekOption,
-} from "../../common/options.js";
+import { chainOption } from "../../common/options.js";
 import { l2Chains } from "../../data/chains.js";
-import { ETH_TOKEN } from "../../utils/constants.js";
-import { bigNumberToDecimal, decimalToBigNumber } from "../../utils/formatters.js";
-import {
-  getAddressFromPrivateKey,
-  getL1Provider,
-  getL2Provider,
-  getL2Wallet,
-  optionNameToParam,
-} from "../../utils/helpers.js";
+import { optionNameToParam } from "../../utils/helpers.js";
 import { Provider } from "zksync-web3";
 import Logger from "../../utils/logger.js";
 import { isTransactionHash } from "../../utils/validators.js";
-import zeek from "../../utils/zeek.js";
 import { Option } from "commander";
 
-const transactionHashOption = new Option("--tx, --transaction <TX_HASH>", "get transaction by hash");
+const transactionHashOption = new Option("--tx-hash, --transaction-hash <TX_HASH>", "get transaction by hash");
 
 type GetTransactionOptions = {
   chain?: string;
@@ -46,7 +29,7 @@ export const handler = async (options: GetTransactionOptions) => {
         },
         {
           message: transactionHashOption.description,
-          name: optionNameToParam(transactionHashOption.long!),
+          name: "txHash",
           type: "input",
           required: true,
           validate: (input: string) => isTransactionHash(input),
@@ -60,45 +43,10 @@ export const handler = async (options: GetTransactionOptions) => {
       ...answers,
     };
 
-    const fromChain = l2Chains.find((e) => e.network === options.chain);
-    console.log(fromChain!.rpcUrl)
-    const l2Provider = new Provider();
+    const chain = l2Chains.find((e) => e.network === options.chain);
+    const l2Provider = new Provider(chain!.rpcUrl);
 
     console.log(await l2Provider.getTransactionDetails(String(options.txHash)));
-
-
-    /*const fromChainLabel = fromChain && !options.l2RpcUrl ? fromChain.name : options.l2RpcUrl ?? "Unknown chain";
-    const toChain = l2Chains.find((e) => e.network === options.chain)?.l1Chain;
-    const toChainLabel = toChain && !options.l1RpcUrl ? toChain.name : options.l1RpcUrl ?? "Unknown chain";
-
-    Logger.info("\nWithdraw:");
-    Logger.info(` From: ${getAddressFromPrivateKey(answers.privateKey)} (${fromChainLabel})`);
-    Logger.info(` To: ${options.recipient} (${toChainLabel})`);
-    Logger.info(` Amount: ${bigNumberToDecimal(decimalToBigNumber(options.amount))} ETH`);
-
-    Logger.info("\nSending withdraw transaction...");
-
-    const l1Provider = getL1Provider(options.l1RpcUrl ?? toChain!.rpcUrl);
-    const l2Provider = getL2Provider(options.l2RpcUrl ?? fromChain!.rpcUrl);
-    const senderWallet = getL2Wallet(options.privateKey, l2Provider, l1Provider);
-
-    const withdrawHandle = await senderWallet.withdraw({
-      to: options.recipient,
-      token: ETH_TOKEN.l1Address,
-      amount: decimalToBigNumber(options.amount),
-    });
-    Logger.info("\nWithdraw sent:");
-    Logger.info(` Transaction hash: ${withdrawHandle.hash}`);
-    if (fromChain?.explorerUrl) {
-      Logger.info(` Transaction link: ${fromChain.explorerUrl}/tx/${withdrawHandle.hash}`);
-    }
-
-    const senderBalance = await l2Provider.getBalance(senderWallet.address);
-    Logger.info(`\nSender L2 balance after transaction: ${bigNumberToDecimal(senderBalance)} ETH`);
-
-    if (options.zeek) {
-      zeek();
-    }*/
   } catch (error) {
     Logger.error("There was an error while withdrawing funds:");
     Logger.error(error);
