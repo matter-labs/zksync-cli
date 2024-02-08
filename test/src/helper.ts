97ffdd4301a711e016ba03cbab88badeaee26f5f
pcheremu <pavel.cheremushkin@gmail.com>
2024-02-08 12:02:42 +0100

    test: update output

--- test/src/helper.ts
+++ test/src/helper.ts
@@ -3,17 +3,8 @@ import { spawnSync } from "child_process";
 
 export function executeCommand(command: string) {
   const result = spawnSync(command, { encoding: 'utf-8', shell: true, stdio: 'pipe' });
-
-  if (result.error) {
-    return {
-      output: '' || result.stderr.trim(),
-      exitCode: result.status || 1, error: result.error
-    };
-  } else {
-    console.log(result.stdout);
-    return {
-      output: result.stdout.trim() || result.stderr.trim(),
-      exitCode: result.status || 0
-    };
-  }
+  return {
+    output: result.stdout.trim() || result.stderr.trim(),
+    exitCode: result.status
+  };
 }
