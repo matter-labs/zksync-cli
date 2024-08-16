f2dc82d3f58f6942e5848e7f4509f010640a082a
Sabrina Ferguson <sf@matterlabs.dev>
2024-08-16 11:40:57 -0400

    chore: format file

--- src/commands/contract/common/options.ts
+++ src/commands/contract/common/options.ts
@@ -1,8 +1,26 @@
 import { Option } from "commander";
 
-export const contractOption = new Option("--contract <0x address>", "Contract address");
-export const methodOption = new Option("--method <someContractMethod(arguments)>", "Contract method to interact with");
-export const argumentsOption = new Option("--args, --arguments <arguments...>", "Arguments");
-export const dataOption = new Option("--data <Encoded transaction data>", "Transaction data (e.g. 0x1234)");
-export const abiOption = new Option("--abi <path/to/abi>", "Contract artifact or ABI file location");
-export const showTransactionInfoOption = new Option("--show-info", "Show transaction request info (e.g. encoded data)");
+export const contractOption = new Option(
+  "--contract <0x address>",
+  "Contract address"
+);
+export const methodOption = new Option(
+  "--method <someContractMethod(arguments)>",
+  "Contract method to interact with"
+);
+export const argumentsOption = new Option(
+  "--args, --arguments <arguments...>",
+  "Arguments"
+);
+export const dataOption = new Option(
+  "--data <Encoded transaction data>",
+  "Transaction data (e.g. 0x1234)"
+);
+export const abiOption = new Option(
+  "--abi <path/to/abi>",
+  "Contract artifact or ABI file location"
+);
+export const showTransactionInfoOption = new Option(
+  "--show-info",
+  "Show transaction request info (e.g. encoded data)"
+);
