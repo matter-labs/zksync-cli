aed448aedccf39e83b1f7abe9d87fd3ea5c2fa1a
Dustin Brickwood <dustinbrickwood204@gmail.com>
2025-05-15 09:34:01 -0500

    fix: addres issue with custom rpc

--- src/commands/bridge/deposit.ts
+++ src/commands/bridge/deposit.ts
@@ -101,7 +101,15 @@ export const handler = async (options: DepositOptions) => {
     const toChain = chains.find((e) => e.network === options.chain);
     const toChainLabel = toChain && !options.rpc ? toChain.name : (options.rpc ?? "Unknown chain");
 
-    const l1Provider = getL1Provider(options.l1Rpc ?? fromChain!.rpcUrl, fromChain!.id);
+    const l1Provider = getL1Provider(
+      options.l1Rpc ??
+        toChain?.rpcUrl ??
+        (() => {
+          throw new Error("You must supply either --chain or --l1-rpc");
+        })(),
+      toChain?.id,
+      Boolean(options.l1Rpc)
+    );
     const l2Provider = getL2Provider(options.rpc ?? toChain!.rpcUrl);
     const senderWallet = getL2Wallet(options.privateKey, l2Provider, l1Provider);
     const token = options.token ? await getTokenInfo(options.token!, l2Provider, l1Provider) : ETH_TOKEN;
